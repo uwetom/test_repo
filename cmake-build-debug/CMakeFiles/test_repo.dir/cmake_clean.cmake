21a04fc4ad7adb61d2cdef245c8d0c296f75dbee
tom <tom.garne@uwe.ac.uk>
2020-10-05 11:26:41 +0100

    initial_commit

---
+++ cmake-build-debug/CMakeFiles/test_repo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_repo.dir/main.cpp.obj"
+  "libtest_repo.dll.a"
+  "test_repo.exe"
+  "test_repo.exe.manifest"
+  "test_repo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_repo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
